a4a17d02035296ca0cecac49a7a150e010e74ffc
conghoan21 <conghoan.hnc@gmail.com>
2020-12-16 12:16:29 +0700

    add file

---
+++ src/main/java/com/example/demo/service/Country/ICountryService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service.Country;
+
+import com.example.demo.model.Country;
+import com.example.demo.service.IService;
+
+public interface ICountryService extends IService< Country> {
+}
